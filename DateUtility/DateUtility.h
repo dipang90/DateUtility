31f048e894ecd5e36864bcc27997d7cb36d3c64e
Dipang <dipangs@erdostechnologies.com>
2018-04-10 15:37:54 +0530

    Initial Commit

---
+++ DateUtility/DateUtility.h
@@ -0,0 +1,19 @@
+//
+//  DateUtility.h
+//  DateUtility
+//
+//  Created by NiviData Technology on 10/04/18.
+//  Copyright © 2018 DipangiOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DateUtility.
+FOUNDATION_EXPORT double DateUtilityVersionNumber;
+
+//! Project version string for DateUtility.
+FOUNDATION_EXPORT const unsigned char DateUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateUtility/PublicHeader.h>
+
+
